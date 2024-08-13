9ba4b9ab6be27c9ac161817549df0b77cd79bfb4
itigertechno <119334306+itigertechno@users.noreply.github.com>
2024-08-13 20:08:28 +0300

    tgapi

--- src/app/(default)/layout.tsx
+++ src/app/(default)/layout.tsx
@@ -5,19 +5,27 @@ import { Header } from '@/widgets/header';
 import { StyledEngineProvider } from '@mui/material';
 import '../globals.css';
 import 'swiper/css';
-
+import WebApp from '@twa-dev/sdk';
 import 'react-circular-progressbar/dist/styles.css';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { inter } from '../fonts';
 import Providers from '../progress.bar';
+import { useEffect } from 'react';
 
 export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	useEffect(() => {
+		if (!window && !WebApp?.initData) return;
+		WebApp.ready();
+		WebApp.expand();
+		WebApp.BackButton.show();
+	}, []);
+
 	return (
 		<html lang='en'>
 			<StyledEngineProvider injectFirst>
